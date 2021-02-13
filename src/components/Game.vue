e6981a99becaa47aa53fab222e2e2acf82e975e1
Arman Kozhakhmetov <kozhakhmetov13@gmail.com>
2021-02-13 12:12:56 +0300

    feat: game table init

---
+++ src/components/Game.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="game">
+    <h2 class="game__info">Round: {{ round }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      round: 1,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+  .game {
+    width: 100%;
+    height: 100%;
+    background-color: green;
+    color: #fff;
+
+    &__info {
+      text-align: right;
+    }
+  }
+</style>
